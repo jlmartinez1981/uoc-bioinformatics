01518c24d00e5e6a2fabe342727ff3bb884f4707
Unknown <jose.martinezperez@gmail.com>
2018-03-05 22:09:30 +0100

    configuración del proyecto

---
+++ TFM/software/bioreports/src/README.md
@@ -0,0 +1,3 @@
+https://inviqa.com/blog/using-typescript-node-js
+https://github.com/jsecademy/webpack-express-typescript
+https://medium.com/@Pierre_anthill/typescript-expressjs-api-with-webpack-4655126d884b
--- TFM/software/bioreports/webpack.config.js
+++ TFM/software/bioreports/webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
   entry: './src/index.js',
   output: {
     filename: 'bioreports.js',
     path: path.resolve(__dirname, 'dist')
-  }
-};
+  },
+  mode:'development'
+},
+{
+  entry: './src/server/server.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'server.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode:'development'
+}];
