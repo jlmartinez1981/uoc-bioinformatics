6acafc5e523b01cd750e5720b11b90975998438f
jmartinez_koder <jose.martinezperez@gmail.com>
2018-03-06 15:36:58 +0100

    añadidos scripts de R

---
+++ TFM/software/bioreports/src/r-scripts/dbSNP.R
@@ -0,0 +1,4 @@
+# Modify this depending on the computer
+workingDirectory = 'C:/Users/jmartiez/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
+setwd(workingDirectory);
+
---
+++ TFM/software/bioreports/src/r-scripts/install-packages.R
@@ -0,0 +1,7 @@
+# Install pakckages
+# https://www.bioconductor.org/install/
+# https://www.biostars.org/p/1332/
+# http://www.ensembl.info/2015/06/01/biomart-or-how-to-access-the-ensembl-data-from-r/
+# https://cran.r-project.org/web/packages/rsnps/rsnps.pdf
+source("http://bioconductor.org/biocLite.R")
+biocLite("biomaRt")
