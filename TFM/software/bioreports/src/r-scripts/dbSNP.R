d82c2460caa2e26af09513ec10be5449d5416adb
Unknown <jose.martinezperez@gmail.com>
2018-03-11 15:09:23 +0100

    installar rentrez y reutils

--- TFM/software/bioreports/src/r-scripts/dbSNP.R
+++ TFM/software/bioreports/src/r-scripts/dbSNP.R
@@ -3,5 +3,14 @@ workingDirectory = 'C:/Users/jmartiez/Documents/uoc-bioinformatics/TFM/software/
 workingDirectory = 'C:/Users/jlmartinez/Desktop/UOC-Bioinformatics/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
 setwd(workingDirectory);
 
-library(biomaRt);
+#library(biomaRt);
+library(reutils);
+library(rentrez);
 
+# available databases rentrez
+entrez_dbs()
+entrez_db_searchable("snp")
+
+#reutils
+pmid <- esearch("asthma", "snp")
+pmid
--- TFM/software/bioreports/src/r-scripts/install-packages.R
+++ TFM/software/bioreports/src/r-scripts/install-packages.R
@@ -3,5 +3,9 @@
 # https://www.biostars.org/p/1332/
 # http://www.ensembl.info/2015/06/01/biomart-or-how-to-access-the-ensembl-data-from-r/
 # https://cran.r-project.org/web/packages/rsnps/rsnps.pdf
-source("http://bioconductor.org/biocLite.R")
-biocLite("biomaRt")
+#source("http://bioconductor.org/biocLite.R")
+#biocLite("biomaRt")
+
+#install entrez utils
+install.packages("reutils")
+install.packages("rentrez")
