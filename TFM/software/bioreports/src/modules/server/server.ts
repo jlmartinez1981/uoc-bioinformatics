ed564080a51a273f51735151abb135ba91e81b1a
Unknown <jose.martinezperez@gmail.com>
2018-03-24 10:57:10 +0100

    upload file logic with formidable npm package

--- TFM/software/bioreports/src/modules/server/server.ts
+++ TFM/software/bioreports/src/modules/server/server.ts
@@ -6,6 +6,8 @@ import * as path from 'path';
 import * as errorHandler from 'errorhandler';
 import * as methodOverride from 'method-override';
 import * as compression from 'compression'; // compresses requests
+import * as formidable from 'formidable';
+import * as fs from 'fs';
 
 /**
  * The server.
@@ -76,20 +78,15 @@ export class Server {
      */
     this.app.use(errorHandler());
 
-    // static  files mapped to a virtual directory
-    this.app.use('/static', express.static(path.join(__dirname, '../../public')));
+    /**
+     * https://coligo.io/building-ajax-file-uploader-with-node/
+     * https://gist.github.com/paambaati/db2df71d80f20c10857d
+     */
 
-    // app = config(app);
-
-    this.app.get('/', function(req: express.Request, res: express.Response) {
-      // res.send('Hello world!');
-      res.sendFile(path.join(__dirname + '../../../views/index.html'));
-  });
-
-  const listeningPort = this.app.get('port');
-  this.app.listen(listeningPort, function() {
-    console.log('Rules Server up: http://localhost:', listeningPort);
-  });
+    const listeningPort = this.app.get('port');
+    this.app.listen(listeningPort, function() {
+      console.log('Rules Server up: http://localhost:', listeningPort);
+    });
   }
 
   /**
@@ -99,6 +96,57 @@ export class Server {
    * @method api
    */
   public routes() {
-    // empty for now
+    // static  files mapped to a virtual directory
+    this.app.use('/static', express.static(path.join(__dirname, '../../public')));
+
+    // index
+    this.app.get('/', function(req: express.Request, res: express.Response) {
+      res.sendFile(path.join(__dirname + '../../../views/index.html'));
+    });
+
+    // upload
+    this.app.post('/upload', function(req, res, next) {
+      console.log('UPLOADING FILE: ', req.params.files); // the uploaded file object
+      next();
+    });
+
+    // upload
+    this.app.post('/upload', function(req, res, next) {
+      console.log('UPLOADING FILE...');
+      // create an incoming form object
+      const form = new formidable.IncomingForm();
+
+      // specify that we want to allow the user to upload multiple files in a single request
+      // form.multiples = true;
+
+      // store all uploads in the /uploads directory
+      form.uploadDir = path.join(__dirname, '/uploads');
+
+      // every time a file has been uploaded successfully,
+      // rename it to it's orignal name
+      form.on('file', function(field, file) {
+        fs.rename(file.path, path.join(form.uploadDir, file.name), (err) => {
+          if (err) {
+            throw err;
+          }
+          console.log('Rename complete!');
+        });
+      });
+
+      // log any errors that occur
+      form.on('error', function(err) {
+        console.log('An error has occured: \n' + err);
+      });
+
+      // once all the files have been uploaded, send a response to the client
+      form.on('end', function() {
+        console.log('FILE UPLOADED: ', req.params.files); // the uploaded file object
+        res.end('success');
+      });
+
+      // parse the incoming request containing the form data
+      form.parse(req);
+
+    });
   }
 }
