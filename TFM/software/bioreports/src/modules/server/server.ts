fe6669fe6ad2887874408d165e12278a5866b8d7
PC-INCLUSITE-10\inclusite <jose.martinezperez@gmail.com>
2018-03-27 10:07:02 +0200

    wip

--- TFM/software/bioreports/src/modules/server/server.ts
+++ TFM/software/bioreports/src/modules/server/server.ts
@@ -66,26 +66,21 @@ export class Server {
    */
   public config() {
     this.app.set('port', process.env.PORT || 3000);
-    this.app.set('views', __dirname + '../../views');
+    this.app.set('views', path.join(__dirname, '../../views'));
+    this.app.set('public', path.join(__dirname, '../../public'));
+
     /**
      * Error Handler. Provides full stack - remove for production
      */
-    this.app.use(errorHandler());
+    if (process.env.NODE_ENV === 'development') {
+      // only use in development
+      this.app.use(errorHandler());
+    }
 
-    // static  files mapped to a virtual directory
-    this.app.use('/static', express.static(path.join(__dirname, '../../public')));
-
-    // app = config(app);
-
-    this.app.get('/', function(req: express.Request, res: express.Response) {
-      // res.send('Hello world!');
-      res.sendFile(path.join(__dirname + '../../../views/index.html'));
-  });
-
-  const listeningPort = this.app.get('port');
-  this.app.listen(listeningPort, function() {
-    console.log('Rules Server up: http://localhost:', listeningPort);
-  });
+    const listeningPort = this.app.get('port');
+    this.app.listen(listeningPort, function() {
+      console.log('Rules Server up: http://localhost:', listeningPort);
+    });
   }
 
   /**
@@ -95,6 +90,12 @@ export class Server {
    * @method api
    */
   public routes() {
-    // empty for now
+    // static  files mapped to a virtual directory
+    this.app.use('/static', express.static(this.app.get('public')));
+
+    const viewPath = this.app.get('views');
+    this.app.get('/', function(req: express.Request, res: express.Response) {
+      res.sendFile(path.join(viewPath, 'index.html'));
+  });
   }
 }
