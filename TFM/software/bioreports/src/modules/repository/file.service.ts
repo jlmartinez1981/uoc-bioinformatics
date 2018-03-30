342ef03a290e4c2867eaf32414087a66ee83d264
Unknown <jose.martinezperez@gmail.com>
2018-03-30 13:34:22 +0200

    definición de métodos en FileService

--- TFM/software/bioreports/src/modules/repository/file.service.ts
+++ TFM/software/bioreports/src/modules/repository/file.service.ts
@@ -6,4 +6,20 @@
  */
 export class FileService {
 
+    /**
+     *
+     * @param filePath
+     * @param fileContents
+     */
+    public saveFile(filePath: string, fileContents: any): boolean {
+        // TODO save file
+        return false;
+    }
+
+    /**
+     *
+     */
+    public getFile(): void {
+        // TODO readFile and return the data
+    }
 }
