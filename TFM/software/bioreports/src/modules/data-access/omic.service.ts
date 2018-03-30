1c8c64da15d79e2f70b2d9585fc15b1c79f6c3b6
Unknown <jose.martinezperez@gmail.com>
2018-03-30 14:24:11 +0200

    omic service singleton

--- TFM/software/bioreports/src/modules/data-access/omic.service.ts
+++ TFM/software/bioreports/src/modules/data-access/omic.service.ts
@@ -6,4 +6,23 @@
  */
 export class OmicService {
 
+    private static instance: OmicService;
+    /**
+     * Constructor.
+     *
+     * @class OmicService
+     * @constructor
+     */
+    constructor() {
+        if (OmicService.instance) {
+            throw 'OmicService instance already exists. Please use OmicService.getInstance()';
+        }
+    }
+
+    public static getInstance(): OmicService {
+        if (OmicService.instance) {
+            return OmicService.instance;
+        }
+        return new OmicService();
+    }
 }
