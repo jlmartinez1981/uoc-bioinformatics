ba67bcb836f959f61efd2618a17f9489763e5377
Unknown <jose.martinezperez@gmail.com>
2018-04-14 19:18:31 +0200

    ancestry study function

---
+++ TFM/software/bioreports/src/r-scripts/admixture-ancestry.R
@@ -0,0 +1,21 @@
+# radmixture ancestry analysis
+# radixture test
+library(radmixture)
+
+# calculate individual ancestry
+ancestry_from_snp_file <- function (path_to_file){
+  path_to_file = 'C:/Users/jlmartinez/Desktop/7210.23andme.5592.txt';
+  genotype <- read.table(file = path_to_file)
+  
+  load('C:/Users/jlmartinez/Desktop/reference-data/globe13.alleles.RData')
+  load('C:/Users/jlmartinez/Desktop/reference-data/globe13.13.F.RData')
+  
+  # Use K13
+  res <- tfrdpub(genotype, 13, globe13.alleles, globe13.13.F)
+  
+  # Use K13
+  ances <- fFixQN(res$g, res$q, res$f, tol = 1e-4, method = "BR", pubdata = "K13")
+  
+  ances$q
+  return(ances$q)
+}
