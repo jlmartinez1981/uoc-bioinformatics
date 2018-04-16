e43613706e79321f68c6705d9a7c711f46da3e39
Unknown <jose.martinezperez@gmail.com>
2018-04-16 20:28:55 +0200

    comenzar en 0

--- TFM/software/bioreports/src/r-scripts/disease-pipeline-test.R
+++ TFM/software/bioreports/src/r-scripts/disease-pipeline-test.R
@@ -1,7 +1,7 @@
 # Modify this depending on the computer
 workingDirectory = 'C:/Users/jmartiez/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
 workingDirectory = 'C:/Users/jlmartinez/Desktop/UOC-Bioinformatics/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
-workingDirectory = 'C:/Users/inclusite/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
+#workingDirectory = 'C:/Users/inclusite/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
 setwd(workingDirectory);
 
 #clean variables
@@ -19,6 +19,8 @@ if(!exists("disease_data_from_snp", mode="function")){
 
 test_upload <- 'C:/Users/jlmartinez/bioreports/upload_processed/test-upload.txt'
 test_upload <- 'C:/Users/inclusite/bioreports/upload_processed/20180416115748-ancestry-7339.ancestry.5702'
+test_upload <- 'C:/Users/jlmartinez/bioreports/upload_processed/20180416195531-disease-anonymous.csv'
+
 rawData <- read.csv(file=test_upload, header=TRUE, sep=" ", stringsAsFactors = FALSE)
 
 tuple_df <- cbind(rawData[,2], rawData[,3])
@@ -45,14 +47,14 @@ data_df <- data.frame(snp_id=character(),
 #(rs1815739, 11:66560624) 5 snp results, 2 omim results, pathologic (sprint performance)
 #(rs6152, X:67545785) 2 snp results, benign
 
-tuple_df <- cbind(c(9,1,19,9,17,11),
-                  c(117713024,82154, 44908684,117713324,39895095,67585218))
+#tuple_df <- cbind(c(9,1,19,9,17,11),
+#                  c(117713024,82154, 44908684,117713324,39895095,67585218))
 
-tuple_df <- cbind(c(6,19,11,'X'),
-                 c(149400554, 44908684,66560624,67545785))
-colnames(tuple_df) <- c("chr_id", "chr_pos")
+#tuple_df <- cbind(c(6,19,11,'X'),
+#                 c(149400554, 44908684,66560624,67545785))
+#colnames(tuple_df) <- c("chr_id", "chr_pos")
 total_rows <- nrow(tuple_df)
-for(i in 1:total_rows){
+for(i in 0:total_rows){
   chr <- tuple_df[i,][[1]]
   pos <- tuple_df[i,][[2]]
   cat(sprintf('PROCESSING %s of %s: %s:%s \n', i, total_rows, chr, pos))
@@ -66,8 +68,8 @@ for(i in 1:total_rows){
 data_df
 
 # TODO save into reports folder
-fileToReport <- 'C:/Users/jlmartinez/bioreports/reports/disease/test.csv'
-fileToReport <- 'C:/Users/inclusite/bioreports/reports/disease/test.csv'
+fileToReport <- 'C:/Users/jlmartinez/bioreports/reports/disease/disease-test.csv'
+# fileToReport <- 'C:/Users/inclusite/bioreports/reports/disease/test.csv'
 # http://rprogramming.net/write-csv-in-r/
 cat(sprintf('SAVING REPORT TO: %s \n', fileToReport))
 write.csv(data_df, file = fileToReport,row.names=FALSE, na = '')
