591e5c2b0cee4e008892b351d79f5dd6df99a870
Unknown <jose.martinezperez@gmail.com>
2018-04-17 20:54:17 +0200

    búsqueda  por rsid

--- TFM/software/bioreports/src/r-scripts/disease-pipeline-test.R
+++ TFM/software/bioreports/src/r-scripts/disease-pipeline-test.R
@@ -1,7 +1,7 @@
 # Modify this depending on the computer
 workingDirectory = 'C:/Users/jmartiez/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
 workingDirectory = 'C:/Users/jlmartinez/Desktop/UOC-Bioinformatics/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
-workingDirectory = 'C:/Users/inclusite/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
+#workingDirectory = 'C:/Users/inclusite/Documents/uoc-bioinformatics/TFM/software/bioreports/src/r-scripts';
 setwd(workingDirectory);
 
 #clean variables
@@ -19,12 +19,12 @@ if(!exists("disease_data_from_snp", mode="function")){
 
 test_upload <- 'C:/Users/jlmartinez/bioreports/upload_processed/test-upload.txt'
 test_upload <- 'C:/Users/inclusite/bioreports/upload_processed/20180416200548-disease-7210.23andme.5592-chr1.csv'
-#test_upload <- 'C:/Users/jlmartinez/bioreports/upload_processed/20180416200548-disease-7210.23andme.5592.csv'
+test_upload <- 'C:/Users/jlmartinez/bioreports/upload_processed/20180416200548-disease-7210.23andme.5592.csv'
 
 rawData <- read.csv(file=test_upload, header=FALSE, sep=" ", stringsAsFactors = FALSE)
 
-tuple_df <- cbind(rawData[,2], rawData[,3])
-colnames(tuple_df) <- c("chr_id", "chr_pos")
+tuple_df <- cbind(rawData[,1], rawData[,2], rawData[,3])
+colnames(tuple_df) <- c("rsid", "chr_id", "chr_pos")
 
 #create empty dataframe https://stackoverflow.com/questions/10689055/create-an-empty-data-frame
 data_df <- data.frame(snp_id=character(),
@@ -47,27 +47,32 @@ data_df <- data.frame(snp_id=character(),
 #(rs1815739, 11:66560624) 5 snp results, 2 omim results, pathologic (sprint performance)
 #(rs6152, X:67545785) 2 snp results, benign
 
-tuple_df <- cbind(c(9,1,19,9,17,11),
-                  c(117713024,82154, 44908684,117713324,39895095,67585218))
+#tuple_df <- cbind(c(9,1,19,9,17,11),
+#                  c(117713024,82154, 44908684,117713324,39895095,67585218))
 
-tuple_df <- cbind(c(6,19,11,'X'),
-                 c(149400554, 44908684,66560624,67545785))
-colnames(tuple_df) <- c("chr_id", "chr_pos")
+#tuple_df <- cbind(c(6,19,11,'X'),
+#                 c(149400554, 44908684,66560624,67545785))
+#colnames(tuple_df) <- c("chr_id", "chr_pos")
 
 total_rows <- nrow(tuple_df)
 cat(sprintf('PROCESSING DATA FRAME WITH %s ROWS \n',total_rows))
-fileToReport <- 'C:/Users/inclusite/bioreports/reports/disease/test.csv'
+fileToReport <- 'C:/Users/jlmartinez/bioreports/reports/disease/test.csv'
+#fileToReport <- 'C:/Users/inclusite/bioreports/reports/disease/test.csv'
 cat(sprintf('SAVING REPORT TO: %s \n', fileToReport))
 write.table(data_df, file = fileToReport, row.names=FALSE, col.names = TRUE, na = '', append = FALSE)
 
 for(i in 1:total_rows){
-  chr <- tuple_df[i,][[1]]
-  pos <- tuple_df[i,][[2]]
-  cat(sprintf('PROCESSING %s of %s: %s:%s \n', i, total_rows, chr, pos))
+  rsid <- tuple_df[i,][[1]]
+  chr <- tuple_df[i,][[2]]
+  pos <- tuple_df[i,][[3]]
+  cat(sprintf('PROCESSING %s of %s: rsid %s at %s:%s \n', i, total_rows, rsid, chr, pos))
   if(!is.na(chr)){
-    subdata_df <- disease_data_from_snp(chr, pos)
-    write.table(subdata_df, file = fileToReport,row.names=FALSE, col.names = FALSE, na = '', append = TRUE)
+    subdata_df <- tryCatch({exp=disease_data_from_snp(rsid, chr, pos)}, error=function(i){
+      cat(sprintf('ERROR PROCESSING rsid:%s at %s:%s \n', rsid, chr, pos))
+      return(NULL)
+    })
     if(!is.null(subdata_df)){
+      write.table(subdata_df, file = fileToReport,row.names=FALSE, col.names = FALSE, na = '', append = TRUE)
       data_df <- rbind(data_df, subdata_df)     
     }
   }  
